2045cb72b6bbdca9b5909011e70df6fa4691a5a4
mtm-chawpwintphyu <mtm.chawpwintphyu@gmail.com>
2025-02-07 08:35:16 +0630

    Delete show.blade.php from posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,14 +7,14 @@
             {{ __('Create Post') }}
         </div>
         <div class="card-body">
-
-            <form  action="{{ route('posts.confirm') }}" method="POST">
+            <form action="{{ route('posts.confirm') }}" method="POST">
                 @csrf
                 <div class="row mb-3">
                     <label for="title" class="col-md-4 col-form-label text-md-end">Title</label>
                     <div class="col-md-4">
                         <input type="text" name="title" id="title"
-                            class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}">
+                            class="form-control @error('title') is-invalid @enderror"
+                            value="{{ $title ?? old('title') }}">
                         @error('title')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
@@ -26,7 +26,7 @@
                     <label for="description" class="col-md-4 col-form-label text-md-end">Description</label>
                     <div class="col-md-4">
                         <textarea name="description" id="description"
-                            class="form-control @error('description') is-invalid @enderror">{{ old('description') }}</textarea>
+                            class="form-control @error('description') is-invalid @enderror">{{ $description ?? old('description') }}</textarea>
                         @error('description')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
